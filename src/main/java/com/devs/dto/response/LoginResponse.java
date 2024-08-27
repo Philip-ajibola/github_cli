9a89844f9f2ae3232ad76900b200ca921e7a7394
philip <ajibolaphilip10@gmail.com>
2024-08-27 16:44:28 +0100

    made a correction in the workflow

--- src/main/java/com/devs/dto/response/LoginResponse.java
+++ src/main/java/com/devs/dto/response/LoginResponse.java
@@ -1,12 +1,12 @@
 package com.devs.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
     private String username;
     private boolean success;
--- src/main/java/com/devs/dto/response/RegisterResponse.java
+++ src/main/java/com/devs/dto/response/RegisterResponse.java
@@ -1,12 +1,12 @@
 package com.devs.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterResponse {
     private Long id;
     private String username;
